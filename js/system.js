c3cf8a9b269215c417a320648b01e7524c5b0025
TheN4rdD0g <grahamsmiller@gmail.com>
2019-03-12 15:15:15 -0400

    add text replacement functionality

--- js/system.js
+++ js/system.js
@@ -1,11 +1,10 @@
-$('#g-input').val();
 $(document).ready(function(){
-	console.log($('#g-input').val());
 	$(document).keypress(function(key) {
 		console.log(key.which);
 		if (key.which == 13) 
 		{
 			var gInput = $('#g-input').val();
+			$('#g-input').val(' ');
 			console.log(gInput);
 			$('#g-output').append("<p>" + gInput + "<p>");
 		}
