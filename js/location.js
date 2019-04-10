70c20ce373b687868d695b21b89e0979c2ce8ec2
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-04-10 11:21:00 -0400

    Fix formatting and links

--- js/location.js
+++ js/location.js
@@ -1,7 +1,39 @@
-{ "name":"John", "age":30, "car":null }
-{ "location":"House", "characters":null, "objects":"Bed, Mirror, PC" }
-{ "location":"Patio", "characters":null, "objects":"Table, Chairs" }
-{ "location":"WacDonalds", "characters":"Employee, Customer", "objects":"Vending Machine, Register" }
-{ "location":"Behind WacDonalds", "characters":"Thief", "objects":"Dumpster" }
-{ "location":"Polls", "characters":null, "objects":"Box" }
-{ "location":"Launchpad", "characters":null, "objects":"Rocket" }
+{ 
+	"name":"John", 
+	"age":30, 
+	"car":null }
+{ 
+	"location":"House",
+ 	"characters":null,
+  	"objects":"Bed, Mirror, PC" 
+}
+
+{ 
+	"location":"Patio", 
+	"characters":null, 
+	"objects":"Table, Chairs" 
+}
+
+{ 
+	"location":"WacDonalds", 
+	"characters":"Employee, Customer", 
+	"objects":"Vending Machine, Register" 
+}
+
+{ 
+	"location":"Behind WacDonalds", 
+	"characters":"Thief", 
+	"objects":"Dumpster" 
+}
+
+{ 
+	"location":"Polls", 
+	"characters":null, 
+	"objects":"Box" 
+}
+
+{ 
+	"location":"Launchpad", 
+	"characters":null, 
+	"objects":"Rocket" 
+}
