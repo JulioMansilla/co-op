733f305d41c2431a9c6d30ea7dde7e013f1b89c8
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-04-10 11:40:05 -0400

    Add character art to json objects

---
+++ js/entities.js
@@ -0,0 +1,63 @@
+{ 
+	"name":"House-Fly", 
+	"location":"House", 
+	"description":"Flying in the House" ,
+	"link":"img/character-images/Character1"
+	"stats":
+}
+
+{ 
+	"name":"Reginald", 
+	"location":"Patio", 
+	"description":"Encouraging you to Pokemon-GO to the polls".
+	"link":"img/character-images/Character2"
+	"stats":
+}
+
+{ 
+	"name":"R2-Flip2", 
+	"location":"WacDonalds", 
+	"description":"Existing only to flip patties",
+	"link":"img/character-images/Character3"
+	"stats":
+}
+
+{ 
+	"name":"Gritty", 
+	"location":"Downtown", 
+	"description":"Encouraging you to seize the means of production",
+	"link":"img/character-images/Character4"
+	"stats":
+}
+
+{ 
+	"name":"Testostero", 
+	"location":"Downtown", 
+	"description":"Warning you about the dangers of soy while offering his items that buff your weapons",
+	"link":"img/character-images/Character5"
+	"stats":
+}
+
+{ 
+	"name":"Employee", 
+	"location":"SpaceX", 
+	"description":"Loving her job" ,
+	"link":"img/character-images/Character6"
+	"stats":
+}
+
+{ 
+	"name":"Thief", 
+	"location":"Behind WacDonalds", 
+	"description":"Telling you about the treasure that can be found in a dumpster" 
+	"link":"img/character-images/Character7"
+	"stats":
+}
+
+{ 
+	"name":"Robot", 
+	"location":"Polls", 
+	"description":"Reminding you that your decision will change the world" 
+	"link":"img/character-images/Character8"
+	"stats":
+}
