5fcd03e6f0ba1055cf96598f70e35a790c61ecba
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-04-30 15:13:58 -0400

    Fix descriptions

--- js/entities.js
+++ js/entities.js
@@ -1,89 +1,50 @@
 var entities = {
 	'House-Fly':
 		{
-			'description':"Flying in the House",
+			'description':"A fly buzzes in the corner, slightly giving you the urge to attack it. You realize that this fly has been here for a while, but are suddenly filled with apathy. [Attack] or [Leave]?",
 			'location':"House",
 			'image':"img/character-images/Character1"
 		},
 	'Reginald':
 		{
-			'description':"Encouraging you to Pokemon-GO to the Polls",
+			'description':"A man with a strange head starts shouting loudly, and encourages you to Pokemon-GO to the Polls. You start to drown him out, but consider talking to him. [Talk] or [Leave]?",
 			'location':"Patio",
 			'image':"img/character-images/Character2"
 		},
 	'R2-Flip2':
 		{
-			'description':"Existing only to flip patties",
+			'description':"A mindless robot that exists only to flip patties. You can't help but feel sorry for it. [Attack] or [Leave]?",
 			'location':"WacDonalds",
 			'image':"img/character-images/Character3"
 		},
 	'Gritty':
 		{
-			'description':"Encouraging you to seize the means of production",
+			'description':"A weird-looking thing encourages you to seize the means of production. You are unsure what to do next.[Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character4"
 		},
 	'Testostero':
 		{
-			'description':"Warning you about the dangers of soy while offering his items that buff your weapons",
+			'description':"A very old-looking man warns you about the dangers of soy while offering his items that buff your weapons. [Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character5"
 		},
 	'Employee':
 		{
-			'description':"Loving her job",
+			'description':"An employee whose soul has been lost to the Cosmos. You could also give her your soul, but no pressure. [Use] or [Leave]?",
 			'location':"SpaceX",
 			'image':"img/character-images/Character6"
 		},
 	'Thief':
 		{
-			'description':"Telling you about the treasures that can be found in a dumpster",
+			'description':"A thief starts telling you about the treasures that can be found in a dumpster while slowly creeping toward you. [Attack], [Talk], or [Leave]",
 			'location':"Behind WacDonalds",
 			'image':"img/character-images/Character7"
 		},
 	'Robot':
 		{
-			'description':"Reminding you that your decision will change the world",
+			'description':"A robot reminds you that the fate of the world rests in your incapable hands. Choose unwisely.",
 			'location':"Polls",
 			'image':"img/character-images/Character8"
 		}
 };
-
-
-
-
-
-
-
-
-{ 
-	"name":"Testostero", 
-	"location":"Downtown", 
-	"description":"Warning you about the dangers of soy while offering his items that buff your weapons",
-	"link":"img/character-images/Character5"
-	"stats":
-}
-
-{ 
-	"name":"Employee", 
-	"location":"SpaceX", 
-	"description":"Loving her job" ,
-	"link":"img/character-images/Character6"
-	"stats":
-}
-
-{ 
-	"name":"Thief", 
-	"location":"Behind WacDonalds", 
-	"description":"Telling you about the treasure that can be found in a dumpster" 
-	"link":"img/character-images/Character7"
-	"stats":
-}
-
-{ 
-	"name":"Robot", 
-	"location":"Polls", 
-	"description":"Reminding you that your decision will change the world" 
-	"link":"img/character-images/Character8"
-	"stats":
-}
