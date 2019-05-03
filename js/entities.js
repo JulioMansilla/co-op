86e5335136403551219314146779f6686da1fd19
TheN4rdD0g <grahamsmiller@gmail.com>
2019-05-03 12:18:53 -0400

    fix image link in entities.js

--- js/entities.js
+++ js/entities.js
@@ -3,48 +3,48 @@ var entities = {
 		{
 			'description':"A fly buzzes in the corner, slightly giving you the urge to attack it. You realize that this fly has been here for a while, but are suddenly filled with apathy. [Attack] or [Leave]?",
 			'location':"House",
-			'image':"img/character-images/Character1"
+			'image':"img/character-images/Character1.png"
 		},
 	'reginald':
 		{
 			'description':"A man with a strange head starts shouting loudly, and encourages you to Pokemon-GO to the Polls. You start to drown him out, but consider talking to him. [Talk] or [Leave]?",
 			'location':"Patio",
-			'image':"img/character-images/Character2"
+			'image':"img/character-images/Character2.png"
 		},
 	'r2-flip2':
 		{
 			'description':"A mindless robot that exists only to flip patties. You can't help but feel sorry for it. [Attack] or [Leave]?",
 			'location':"WacDonalds",
-			'image':"img/character-images/Character3"
+			'image':"img/character-images/Character3.png"
 		},
 	'gritty':
 		{
 			'description':"A weird-looking thing encourages you to seize the means of production. You are unsure what to do next.[Talk] or [Leave]?",
 			'location':"Downtown",
-			'image':"img/character-images/Character4"
+			'image':"img/character-images/Character4.png"
 		},
 	'testostero':
 		{
 			'description':"A very old-looking man warns you about the dangers of soy while offering his items that buff your weapons. [Talk] or [Leave]?",
 			'location':"Downtown",
-			'image':"img/character-images/Character5"
+			'image':"img/character-images/Character5.png"
 		},
 	'employee':
 		{
 			'description':"An employee whose soul has been lost to the Cosmos. You could also give her your soul, but no pressure. [Use] or [Leave]?",
 			'location':"SpaceX",
-			'image':"img/character-images/Character6"
+			'image':"img/character-images/Character6.png"
 		},
 	'thief':
 		{
 			'description':"A thief starts telling you about the treasures that can be found in a dumpster while slowly creeping toward you. [Attack], [Talk], or [Leave]",
 			'location':"Behind WacDonalds",
-			'image':"img/character-images/Character7"
+			'image':"img/character-images/Character7.png"
 		},
 	'robot':
 		{
 			'description':"A robot reminds you that the fate of the world rests in your incapable hands. Choose unwisely.",
 			'location':"Polls",
-			'image':"img/character-images/Character8"
+			'image':"img/character-images/Character8.png"
 		}
 };
