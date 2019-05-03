7a02d725d00889578dd7aa0ea897c3ddf6d3388f
TheN4rdD0g <grahamsmiller@gmail.com>
2019-05-03 12:14:45 -0400

    change entity keys to lowercase in entities.js

--- js/entities.js
+++ js/entities.js
@@ -1,47 +1,47 @@
 var entities = {
-	'House-Fly':
+	'house-fly':
 		{
 			'description':"A fly buzzes in the corner, slightly giving you the urge to attack it. You realize that this fly has been here for a while, but are suddenly filled with apathy. [Attack] or [Leave]?",
 			'location':"House",
 			'image':"img/character-images/Character1"
 		},
-	'Reginald':
+	'reginald':
 		{
 			'description':"A man with a strange head starts shouting loudly, and encourages you to Pokemon-GO to the Polls. You start to drown him out, but consider talking to him. [Talk] or [Leave]?",
 			'location':"Patio",
 			'image':"img/character-images/Character2"
 		},
-	'R2-Flip2':
+	'r2-flip2':
 		{
 			'description':"A mindless robot that exists only to flip patties. You can't help but feel sorry for it. [Attack] or [Leave]?",
 			'location':"WacDonalds",
 			'image':"img/character-images/Character3"
 		},
-	'Gritty':
+	'gritty':
 		{
 			'description':"A weird-looking thing encourages you to seize the means of production. You are unsure what to do next.[Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character4"
 		},
-	'Testostero':
+	'testostero':
 		{
 			'description':"A very old-looking man warns you about the dangers of soy while offering his items that buff your weapons. [Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character5"
 		},
-	'Employee':
+	'employee':
 		{
 			'description':"An employee whose soul has been lost to the Cosmos. You could also give her your soul, but no pressure. [Use] or [Leave]?",
 			'location':"SpaceX",
 			'image':"img/character-images/Character6"
 		},
-	'Thief':
+	'thief':
 		{
 			'description':"A thief starts telling you about the treasures that can be found in a dumpster while slowly creeping toward you. [Attack], [Talk], or [Leave]",
 			'location':"Behind WacDonalds",
 			'image':"img/character-images/Character7"
 		},
-	'Robot':
+	'robot':
 		{
 			'description':"A robot reminds you that the fate of the world rests in your incapable hands. Choose unwisely.",
 			'location':"Polls",
