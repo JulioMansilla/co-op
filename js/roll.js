cdf43e4f843925b784e7969c54547fce58e328a1
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-05-15 11:13:22 -0400

    Fix roll

--- js/roll.js
+++ js/roll.js
@@ -1,12 +1,6 @@
 function roll(num) {
-	var return = Math.floor(Math.random(1, num) * 19 + 1); 
+	var output = Math.floor(Math.random()* (num - 1) + 1); 
 	// Math.floor rounds the number down to the nearest integer
-	return return;
-}
-
-function roll2(num) {
-	console.log(parseInt(num));
-	var return = Math.floor(Math.random(1, num) * 19 + 1); 
-	// Math.floor rounds the number down to the nearest integer
-	return return;
+	console.log(output);
+	return output;
 }
