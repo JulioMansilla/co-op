1a8a3eab816e43a958b3070015acd08cdcb04ce4
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-05-15 11:15:07 -0400

    Fix previous commit

--- js/roll.js
+++ js/roll.js
@@ -1,6 +1,5 @@
 function roll(num) {
 	var output = Math.floor(Math.random()* (num - 1) + 1); 
 	// Math.floor rounds the number down to the nearest integer
-	console.log(output);
 	return output;
 }
