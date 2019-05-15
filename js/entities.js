89f0bfbfdfb8de53813392cd2326b575bef08a5f
RyanStartedTheFire <frontkickbackflip@gmail.com>
2019-05-15 11:05:53 -0400

    Add stats

--- js/entities.js
+++ js/entities.js
@@ -1,50 +1,135 @@
 var entities = {
+	'cliff':
+		{
+			'description':"";
+			'stats':
+				{	
+					'Strength':"16",
+					'Dexterity':"16",
+					'Constitution':"16",
+					'Intelligence':"10",
+					'Wisdom':"10",
+					'Luck':"10"
+				}
+		},
 	'house-fly':
 		{
 			'description':"A fly buzzes in the corner, slightly giving you the urge to attack it. You realize that this fly has been here for a while, but are suddenly filled with apathy. [Attack] or [Leave]?",
 			'location':"House",
 			'image':"img/character-images/Character1.png"
+			'stats':
+				{	
+					'Strength':"20",
+					'Dexterity':"20",
+					'Constitution':"20",
+					'Intelligence':"20",
+					'Wisdom':"20",
+					'Luck':"20"
+				}
 		},
 	'reginald':
 		{
 			'description':"A man with a strange head starts shouting loudly, and encourages you to Pokemon-GO to the Polls. You start to drown him out, but consider talking to him. [Talk] or [Leave]?",
 			'location':"Patio",
 			'image':"img/character-images/Character2.png"
+			'stats':
+				{	
+					'Strength':"12",
+					'Dexterity':"12",
+					'Constitution':"14",
+					'Intelligence':"12",
+					'Wisdom':"12",
+					'Luck':"18"
+				}
 		},
 	'r2-flip2':
 		{
 			'description':"A mindless robot that exists only to flip patties. You can't help but feel sorry for it. [Attack] or [Leave]?",
 			'location':"WacDonalds",
 			'image':"img/character-images/Character3.png"
+			'stats':
+				{	
+					'Strength':"14",
+					'Dexterity':"18",
+					'Constitution':"7",
+					'Intelligence':"18",
+					'Wisdom':"7",
+					'Luck':"9"
+				}
 		},
 	'gritty':
 		{
 			'description':"A weird-looking thing encourages you to seize the means of production. You are unsure what to do next.[Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character4.png"
+			'stats':
+				{	
+					'Strength':"8",
+					'Dexterity':"8",
+					'Constitution':"8",
+					'Intelligence':"12",
+					'Wisdom':"18",
+					'Luck':"18"
+				}
 		},
 	'testostero':
 		{
 			'description':"A very old-looking man warns you about the dangers of soy while offering his items that buff your weapons. [Talk] or [Leave]?",
 			'location':"Downtown",
 			'image':"img/character-images/Character5.png"
+			'stats':
+				{	
+					'Strength':"18",
+					'Dexterity':"8",
+					'Constitution':"18",
+					'Intelligence':"8",
+					'Wisdom':"12",
+					'Luck':"8"
+				}
 		},
 	'employee':
 		{
 			'description':"An employee whose soul has been lost to the Cosmos. You could also give her your soul, but no pressure. [Use] or [Leave]?",
 			'location':"SpaceX",
 			'image':"img/character-images/Character6.png"
+			'stats':
+				{	
+					'Strength':"8",
+					'Dexterity':"10",
+					'Constitution':"16",
+					'Intelligence':"18",
+					'Wisdom':"8",
+					'Luck':"15"
+				}
 		},
 	'thief':
 		{
 			'description':"A thief starts telling you about the treasures that can be found in a dumpster while slowly creeping toward you. [Attack], [Talk], or [Leave]",
 			'location':"Behind WacDonalds",
 			'image':"img/character-images/Character7.png"
+			'stats':
+				{	
+					'Strength':"15",
+					'Dexterity':"10",
+					'Constitution':"10",
+					'Intelligence':"7",
+					'Wisdom':"16",
+					'Luck':"18"
+				}
 		},
 	'robot':
 		{
 			'description':"A robot reminds you that the fate of the world rests in your incapable hands. Choose unwisely.",
 			'location':"Polls",
 			'image':"img/character-images/Character8.png"
+			'stats':
+				{	
+					'Strength':"7",
+					'Dexterity':"7",
+					'Constitution':"7",
+					'Intelligence':"7",
+					'Wisdom':"7",
+					'Luck':"7"
+				}
 		}
 };
---
+++ js/roll.js
@@ -0,0 +1,12 @@
+function roll(num) {
+	var return = Math.floor(Math.random(1, num) * 19 + 1); 
+	// Math.floor rounds the number down to the nearest integer
+	return return;
+}
+
+function roll2(num) {
+	console.log(parseInt(num));
+	var return = Math.floor(Math.random(1, num) * 19 + 1); 
+	// Math.floor rounds the number down to the nearest integer
+	return return;
+}
